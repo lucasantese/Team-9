4cecb24118162b64c2435a228313a8a6417435a2
ProdigyProGamingLaptop <54320992+ProdigyProGaming@users.noreply.github.com>
2020-02-02 12:30:13 +0000

    Revert "Second push for setup"
    This reverts commit e9af139b15688077e5e63fe0ed468f6d20e7167c.

--- app/src/main/java/com/example/atncl/MainActivity.java
+++ app/src/main/java/com/example/atncl/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.atncl;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
